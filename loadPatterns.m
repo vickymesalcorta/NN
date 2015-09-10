1052e1611c524d39388f4c14796fc653921c650b
vickymesalcorta <m_vickymesalcorta@hotmail.com>
2015-09-10 03:58:01 -0300

    parametros adaptativos y momentum

--- loadPatterns.m
+++ loadPatterns.m
@@ -1,22 +1,22 @@
 function p = loadPatterns(params)
 
-% TODO VER QUE HAY EN ORDERED SAMPLES.
-% NO SE COMO CREAR DESDE OCTAVE ESE ARCHIVO
-% A ESE LO CREAMOS CUANDO TENIA MATLAB
-% POR AHORA LO USO PARA NO PERDER TIEMPO EN ESO
-load('orderedSamples.mat');
+	% TODO VER QUE HAY EN ORDERED SAMPLES.
+	% NO SE COMO CREAR DESDE OCTAVE ESE ARCHIVO
+	% A ESE LO CREAMOS CUANDO TENIA MATLAB
+	% POR AHORA LO USO PARA NO PERDER TIEMPO EN ESO
+	load('orderedSamples.mat');
 
-params.trainingInput = zeros(2, params.training);
-params.trainingInput = x(1:params.training,1:2)';
+	params.trainingInput = zeros(2, params.training);
+	params.trainingInput = x(1:params.training,1:2)';
 
-params.trainingExpected = zeros(1, params.training);
-params.trainingExpected = x(1:params.training,3)';
+	params.trainingExpected = zeros(1, params.training);
+	params.trainingExpected = x(1:params.training,3)';
 
-params.testInput = zeros(2, params.test);
-params.testInput = x(params.training+1:params.patterns,1:2)';
+	params.testInput = zeros(2, params.test);
+	params.testInput = x(params.training+1:params.patterns,1:2)';
 
-params.testExpected = zeros(1, params.test);
-params.testExpected = x(params.training+1:params.patterns,3)';
+	params.testExpected = zeros(1, params.test);
+	params.testExpected = x(params.training+1:params.patterns,3)';
 
-p = params;
+	p = params;
 end
--- main.m
+++ main.m
@@ -1,37 +1,45 @@
 function m = main(actFunct, expError, eta, alpha, adaptInc, adaptDec, adaptStep, arq)
 
-  %load all the parameters in a structure
+    %load all the parameters in a structure
 
-  % DEFINES:
-  % params.patterns: cantidad de patterns totales
-  % params.training: cantidad de pattern para entrenar
-  % beta: beta con el que se define g(x)
-  % maxEpocs
-  % noise
-  gBeta = 1;
-  params.maxEpocs = 1000;
-  params.maxBadSteps = 10;
-  params.noise = 0.0001;
-  % 441 patterns totales, uso menos para poder debuggear
-  params.patterns = 10;
-  params.training = 8;  
-  % END_DEFINES
+    % DEFINES:
+    % params.patterns: cantidad de patterns totales
+    % params.training: cantidad de pattern para entrenar
+    % beta: beta con el que se define g(x)
+    % maxEpocs
+    % noise
+    gBeta = 1;
+    params.maxEpocs = 1000;
+    params.maxBadSteps = 10;
+    params.noise = 0.0001;
+    % 441 patterns totales, uso menos para poder debuggear
+    params.patterns = 10;
+    params.training = 8;  
+    % END_DEFINES
 
-  params.test = params.patterns - params.training;
-  params.expError = expError;
-  params.eta = eta;
-  params.alpha = alpha;
-  params.adaptInc = adaptInc;
-  params.adaptDec = adaptDec;
-  params.adaptStep = adaptStep;
-  params.arq = arq;
-  % Ej: Arq = [2,4,5,1] layers = 3
-  params.layers = size(arq, 2) - 1;
-  % Load weights, patterns and activation function
-  params.w = initWeights(arq);
-  params = loadPatterns(params);
-  params = loadActivationFunction(params, actFunct, gBeta);
+    params.test = params.patterns - params.training;
+    params.expError = expError;
+    params.eta = eta;
+    params.alpha = alpha;
+    params.adaptInc = adaptInc;
+    params.adaptDec = adaptDec;
+    params.adaptStep = adaptStep;
+    params.arq = arq;
+    % Ej: Arq = [2,4,5,1] layers = 3
+    params.layers = size(arq, 2) - 1;
+    % Load weights, patterns and activation function
+    params.w = initWeights(arq);
+    params = loadPatterns(params);
+    params = loadActivationFunction(params, actFunct, gBeta);
 
-  trainedNetwork = trainNetwork(params);
+    trainedNetwork = trainNetwork(params);
 
+    disp('Finalizo luego de epocas: ');
+    disp(trainedNetwork.epocs);
+    disp('con un error de: ');
+    disp(trainedNetwork.error(trainedNetwork.iter-1));
+    disp('el eta vale: ');
+    disp(trainedNetwork.eta);
+
+    % Agregar porcentajes de acierto etc.
 end
