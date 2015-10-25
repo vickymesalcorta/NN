def61ae49e093dae4b0311a0bec7e8caf0a4e848
lulirez <lulirez@gmail.com>
2015-10-25 19:48:34 -0300

    limpieza de codigo

--- loadPatterns.m
+++ loadPatterns.m
@@ -6,9 +6,6 @@ function p = loadPatterns(params,n,actFunct)
 	% POR AHORA LO USO PARA NO PERDER TIEMPO EN ESO
     
     x = orderTemporalSerie(n,params.patterns);
-    
-      save(['serie.mat'], 'x');
-    
     x = normalize(x,actFunct);
     
 	params.trainingInput = zeros(n, params.training);
@@ -19,20 +16,20 @@ function p = loadPatterns(params,n,actFunct)
     
     %PARA ENSEÑAR ELIMINO EL 90% DE LOS VALORES QUE SE ENCUENTREN EN LA
     %FRANJA MEDIA PARA QUE NO PESEN TANTO
-    i = 1;
-    while i < params.training;
-        exp = abs(params.trainingExpected(:,i));
-        if  exp < 0.6
-            if rand > 1
-                params.trainingExpected(:,i) = [];
-                params.trainingInput(:,i)= [];
-                params.patterns = params.patterns -1;
-                params.training = params.training -1;    
-                i = i - 1;
-            end 
-        end
-        i = i + 1;
-    end
+    % i = 1;
+    % while i < params.training;
+    %     exp = abs(params.trainingExpected(:,i));
+    %     if  exp < 0.6
+    %         if rand > 1
+    %             params.trainingExpected(:,i) = [];
+    %             params.trainingInput(:,i)= [];
+    %             params.patterns = params.patterns -1;
+    %             params.training = params.training -1;    
+    %             i = i - 1;
+    %         end 
+    %     end
+    %     i = i + 1;
+    % end
     
     params.testInput = zeros(n, params.test);
 	params.testInput = x(params.training+1:params.patterns-n,1:n)';
